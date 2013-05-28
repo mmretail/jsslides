d3de2c9000f1733d8ad03c653352e497b38e418d
Ondrej Zara <ondrej.zara@gmail.com>
2013-05-28 22:00:55 +0200

    css simplifications

---
+++ v3/slides/help/module.js
@@ -0,0 +1,88 @@
+(function() {
+	var active = false;
+	var node = document.createElement("div");
+	node.id = "help";
+	node.className = "hidden";
+	document.body.appendChild(node);
+	
+	var keyMap = {
+		8: "Backspace",
+		32: "Space",
+		33: "PgUp",
+		34: "PgDown",
+		35: "End",
+		36: "Home",
+		
+		37: "←",
+		38: "↑",
+		39: "→",
+		40: "↓",
+		
+		
+		
+	};
+	
+	var build = function() {
+		var str = "";
+		var listeners = Slides.getKeyListeners();
+		var rows = [];
+
+		for (var i=0;i<listeners.length;i++) {
+			var listener = listeners[i];
+			var label = listener.label;
+			if (!label) { continue; }
+			var index = -1;
+			for (var j=0;j<rows.length;j++) {
+				if (rows[j].label == label) { index = j; }
+			}
+			if (index == -1) {
+				index = rows.length;
+				rows.push({label:label, keys:[]});
+			}
+			rows[index].keys = rows[index].keys.concat(listener.keys);
+		}
+		
+		for (var i=0;i<rows.length;i++) {
+			str += "<tr><td>" + rows[i].label + "</td><td>";
+			var keys = rows[i].keys;
+			for (var j=0;j<keys.length;j++) {
+				var key = keys[j];
+				if (typeof(key) == "string") { continue; }
+				keys[j] = keyMap[key] || String.fromCharCode(key);
+			}
+			str += keys.join(" / ");
+			str += "</td></tr>";
+		}
+
+		return str;
+	}
+	
+	var open = function() {
+		if (active) { return; }
+		active = true;
+		
+		var url = location.href;
+		var hash = url.lastIndexOf("#");
+		if (hash > -1) { url = url.substring(0, hash); }
+		
+		var str = "<h2>" + url + "</h2><table>" + build();
+		str += "</table><p>This is <a href='http://ondras.zarovi.cz/slides/'>Slides v3</a>, © 2013&ndash;" + (new Date().getFullYear()) + " <a href='http://ondras.zarovi.cz/'>Ondřej Žára</a></p>";
+
+		node.innerHTML = str;
+		node.classList.toggle("hidden");
+	}
+	
+	var close = function() {
+		if (!active) { return; }
+		active = false;
+		node.classList.toggle("hidden");
+	}
+
+	var toggle = function() {
+		active ? close() : open();
+	}
+	
+	Slides.addKeyListener(toggle, "?", "Toggle help");
+	Slides.addKeyListener(close, 27);
+	Slides.addStylesheet("help/module.css");
+})();
---
+++ v3/slides/slides3.js
@@ -0,0 +1,245 @@
+document.documentElement.style.display = "none";
+var Slides = {
+	modules: {},
+	slides: [],
+	current: null,
+
+	_path: [].slice.call(document.querySelectorAll("script")).pop().src.split("/").slice(0, -1).join("/"),
+	_listeners: {
+		key: [],
+		change: []
+	},
+
+	next: function() {
+		var result = this.current.next();
+		if (!result) {
+			var index = this.slides.indexOf(this.current) + 1;
+			if (index == this.slides.length) { return; }
+			this.show(this.slides[index]);
+		}
+	},
+
+	prev: function() {
+		var result = this.current.prev();
+		if (!result) { 
+			var index = this.slides.indexOf(this.current) - 1;
+			if (index == -1) { return; }
+			this.show(this.slides[index], true); 
+		}
+
+	},
+
+	show: function(slide, expandAll) {
+		this.current = slide;
+
+		for (var i=0;i<this.slides.length;i++) {
+			var slide = this.slides[i];
+			if (this.current == slide) {
+				slide.show(expandAll);
+			} else {
+				slide.hide();
+			}
+		}
+
+		var event = {type:"change"};
+		for (var i=0;i<this._listeners.change.length;i++) { this._listeners.change[i](event); }
+	},
+
+	addScript: function(path) {
+		var script = document.createElement("script");
+		script.src = this._path + "/" + path; 
+		document.body.appendChild(script);
+		return script;
+	},
+
+	addStylesheet: function(path) {
+		var link = document.createElement("link");
+		link.rel = "stylesheet";
+		link.href = this._path + "/" + path;
+		document.body.appendChild(link);
+		return link;
+	},
+
+	addKeyListener: function(listener, keys, label) {
+		this._listeners.key.push({
+			listener: listener,
+			keys: [].concat(keys),
+			label: label
+		});
+	},
+
+	addChangeListener: function(listener) {
+		this._listeners.change.push(listener);
+	},
+
+	getKeyListeners: function() {
+		return this._listeners.key;
+	},
+	
+	format: function(template) {
+		var replacements = {
+			"s": this.current.getNode().querySelector("h2, h3, strong, em").innerHTML,
+			"t": document.body.querySelector("h1").innerHTML,
+			"n": this.slides.indexOf(Slides.current) + 1,
+			"c": this.slides.length
+		};
+		return template.replace(/%([a-z])/g, function(match, letter) {
+			return replacements[letter] || match;
+		});
+	},
+
+	handleEvent: function(e) {
+		switch (e.type) {
+			case "load":
+				var meta = document.querySelector("meta[name=viewport]");
+				if (!meta) {
+					var meta = document.createElement("meta");
+					meta.name = "viewport";
+					document.querySelector("head, body").appendChild(meta);
+				}
+				meta.content = "user-scalable=0,initial-scale=1";
+
+				this.addStylesheet("slides3.css");
+
+				var nodes = document.querySelectorAll(".slide");
+				for (var i=0;i<nodes.length;i++) {
+					this.slides.push(new Slide(nodes[i]));
+				}
+
+				this.show(this.slides[0]);
+
+				var count = 0;
+				for (var id in this.modules) {
+					count++;
+					this.addScript(id + "/module.js").onload = function() {
+						count--;
+						if (!count) { document.documentElement.style.display = ""; }
+					}
+				}
+			break;
+
+			case "keydown":
+			case "keypress":
+				if (e.keyCode == 9) { 
+					e.preventDefault();
+					return;
+				}
+				for (var i=0;i<this._listeners.key.length;i++) {
+					var item = this._listeners.key[i];
+					var code = e.keyCode;
+					if (e.type == "keypress") { code = String.fromCharCode(e.charCode).toUpperCase(); }
+					if (item.keys.indexOf(code) != -1) { item.listener(e); }
+				}
+			break;
+		} /* switch */
+	} /* handleEvent */
+};
+window.addEventListener("load", Slides);
+document.addEventListener("keydown", Slides);
+document.addEventListener("keypress", Slides);
+
+var Slide = function(node) {
+	this._node = node;
+
+	this._sections = [];
+	this._index = -1;
+	
+	this._findSections(node);
+	this.next(); /* show first section */
+}
+
+Slide.prototype.getNode = function() {
+	return this._node;
+}
+
+Slide.prototype._findSections = function(node) {
+	if (node.classList.contains("section")) { 
+		this._sections.push(node);
+	}
+
+	var hasSections = node.classList.contains("sections");
+	node.classList.remove("sections");
+	
+	for (var i=0;i<node.children.length;i++) {
+		var child = node.children[i];
+		if (hasSections) { child.classList.add("section"); }
+		this._findSections(child);
+	}
+}
+
+Slide.prototype.hide = function() {
+	this._node.classList.remove("current");
+}
+
+Slide.prototype.show = function(expandAll) {
+	this._node.classList.add("current");
+
+	this._index = (expandAll ? this._sections.length-1 : 0);
+	for (var i=0;i<this._sections.length;i++) {
+		var section = this._sections[i];
+		if (i == this._index) {
+			section.classList.add("current");
+		} else {
+			section.classList.remove("current");
+		}
+	}
+}
+
+Slide.prototype.next = function() {
+	if (this._index+1 >= this._sections.length) { return false; }
+	this._index++;
+	this._syncSections();
+	return true;
+}
+
+Slide.prototype.prev = function() {
+	if (this._index <= 0) { return false; }
+	this._index--;
+	this._syncSections();
+	return true;
+}
+
+Slide.prototype._syncSections = function() {
+	for (var i=0;i<this._sections.length;i++) {
+		var section = this._sections[i];
+		if (i == this._index) {
+			section.classList.add("current");
+			section.classList.remove("after");
+		} else {
+			section.classList.remove("current");
+			if (i < this._index) {
+				section.classList.remove("after");
+			} else {
+				section.classList.add("after");
+			}
+		}
+	}
+}
+
+/* default module configuration */
+Slides.modules.keyboard = true;
+Slides.modules.url = true;
+Slides.modules.title = "(%n) %t"; /* %t %s %n %c" */
+Slides.modules.progress = {
+	template: "%n/%c",
+	parent: "footer"
+};
+Slides.modules.time = {
+	length: 10*60*1000,
+	parent: "footer"
+};
+Slides.modules.skin = "seznam";
+Slides.modules.language = ["en"];
+Slides.modules.transition = "horizontal"; /* none vertical horizontal blend corner */
+Slides.modules.overview = true;
+Slides.modules.help = true;
+Slides.modules.fontsize = {
+	sizes: ["90%", "100%" ,"120%", "150%", "200%", "250%", "350%"],
+	normal: "150%"
+};
+Slides.modules.syntax = true;
+Slides.modules.touch = {
+	tap: true,
+	swipe: true,
+	pinch: true
+};
